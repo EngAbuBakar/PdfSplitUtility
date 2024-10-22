4c749eead09fc42e28d89be98e45f80981c56203
Muhammad.Abubakar <abubakaryameen0243@gmail.com>
2024-10-22 17:18:10 +0500

    print only odd element from the list

--- src/main/java/com/Pdf/component/VectorExample.java
+++ src/main/java/com/Pdf/component/VectorExample.java
@@ -1,6 +1,9 @@
 package com.Pdf.component;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.Vector;
+import java.util.stream.Collectors;
 
 public class VectorExample {
     public static void main(String[] args) {
@@ -8,8 +11,10 @@ public class VectorExample {
 
         list.add(2);
         list.add(1);
+        list.add(5);
+        list.add(78);
 
-        long count = list.stream().count();
+        List<Integer> count =  list.stream().filter(x->x%2!=0).collect(Collectors.toList());
         System.out.println(count);
 
     }
