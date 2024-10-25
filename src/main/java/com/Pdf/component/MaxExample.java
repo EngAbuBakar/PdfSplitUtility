3850404765f4091cb4cfb8e649773337f24db42b
Muhammad.Abubakar <abubakaryameen0243@gmail.com>
2024-10-25 15:13:55 +0500

    revamp the code

--- src/main/java/com/Pdf/component/MaxExample.java
+++ src/main/java/com/Pdf/component/MaxExample.java
@@ -3,25 +3,23 @@ package com.Pdf.component;
 public class MaxExample {
 
     public static void main(String[] args) {
-        int[] array = {2,4,54,5};
+        int[] array = {233,4,545,5, -355,2};
 
         int max= 0;
-        System.out.println("The maximum number is: ");
+
         for (int i = 0 ; i< array.length; i++){
             if(array[i]>max){
                 max =array[i];
             }
         }
-        System.out.println(max);
+        System.out.println("The maximum number is: "+max);
 
-
-        System.out.println("The minimum number is: ");
         for (int i = 0 ; i< array.length; i++){
             if(array[i]<max){
                 max =array[i];
             }
         }
-        System.out.println(max);
+        System.out.println( "The minimum number is: "+max);
     }
 
 
